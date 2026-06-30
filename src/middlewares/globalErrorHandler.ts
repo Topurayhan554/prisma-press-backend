a6a129cccdaf7aca36323cae0c2176a046389e12
Topurayhan554 <topurayhantipu@gmail.com>
2026-06-30 12:42:25 +0600

    updated errorHandler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import httpStatus from "http-status";
 import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client";
 
 export const globalErrorHandler = (
   err: any,
@@ -7,10 +8,47 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+  let statusCode: number;
+  statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+
+  let errorMessage = err.message || "Internal Server Error";
+  let errorName = err.name || "Internal Server Error";
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    statusCode = httpStatus.BAD_REQUEST;
+    errorMessage = "You have provided incorrect field type or missing fields";
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      statusCode = httpStatus.BAD_REQUEST;
+
+      errorMessage = "Duplicate key error";
+    } else if (err.code === "P2025") {
+      statusCode = httpStatus.NOT_FOUND;
+      errorMessage =
+        "An operation failed because it depends on one or more records that were required but not found.";
+    } else if (err.code === "P2003") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage = "Foreign Key Constraint Failed";
+    }
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    if (err.errorCode === "P1000") {
+      statusCode = httpStatus.UNAUTHORIZED;
+      errorMessage =
+        "Authentication failed again database server. Please check your credentials";
+    } else if (err.errorCode === "P1001") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage = "Can't reach database server";
+    }
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    errorMessage = "Error Occurred during query execution";
+  }
+
+  res.status(statusCode).json({
     success: false,
-    statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-    message: err.message,
-    error: err.stack,
+    statusCode,
+    name: errorName,
+    message: errorMessage,
+    error: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
 };
